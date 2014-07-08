d05beca99b32e5380baa174e250257543b537ac6
Anton Razumovskyy <anton.razumovskyy@gmail.com>
2014-07-08 22:29:45 +0300

    add main.js, first release is ready

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/**
+ * @fileoverview Main file fo the game
+ * @author anton.razumovskyy@gmail.com
+ */
+"use strict";
+$().ready(function () {
+	main();
+});
+
+function main() {
+	console.log("main ...")
+	var game = new Game();
+}
